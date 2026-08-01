b923f07e72ce8337971254c7e8bf05a629afb6f8
Shubham Joshi <45368598+shubham-1123@users.noreply.github.com>
2026-08-01 11:11:04 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res = "";
+        int i =0, j =0;
+        while(i < word1.size() && j < word2.size()){
+            res = res + word1[i] + word2[j];
+            i ++, j ++;
+        }
+        while(i < word1.size()){
+            res += word1[i];
+            i ++;
+        }
+        while(j < word2.size()){
+            res += word2[j];
+            j ++;
+        }
+        return res;
+    }
+};
