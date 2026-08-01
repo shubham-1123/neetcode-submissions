ed1ffdf37316747f7586e703376040d0574c69ba
Shubham Joshi <45368598+shubham-1123@users.noreply.github.com>
2026-08-01 11:05:16 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        
+        auto palindrome = [](const string& s, int left, int right){
+            while(left < right){
+                if(s[left] != s[right])return false;
+                left ++, right--;
+            }
+            return true;
+        };
+        
+        int left = 0, right = s.size()-1;
+        while(left < right){
+            if(s[left] != s[right]){
+                return palindrome(s, left+1, right) || palindrome(s, left, right-1);
+            }
+            left ++, right--;
+        }
+        return true;
+    }
+};
