e1f46b55aca0763a5a8e1ccf1054951dbae9617c
Shubham Joshi <45368598+shubham-1123@users.noreply.github.com>
2026-08-20 22:00:57 +0530

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if(intervals.size() <= 1)return true;
+        sort(intervals.begin(), intervals.end(), [](Interval& p1, Interval& p2){
+            return (p1.start < p2.start);
+        });
+
+        for(int i=0;i<intervals.size()-1;i++){
+            if(intervals[i].end > intervals[i+1].start)return false;
+        }
+        return true;
+    }
+};
