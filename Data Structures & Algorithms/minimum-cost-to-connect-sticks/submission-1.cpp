94770db1a98bc026f623b1033712d74f5f9b5caf
Shubham Joshi <45368598+shubham-1123@users.noreply.github.com>
2026-09-04 23:18:05 +0530

    Add: minimum-cost-to-connect-sticks - submission-1

---
+++ Data Structures & Algorithms/minimum-cost-to-connect-sticks/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int connectSticks(vector<int>& sticks) {
+        if(sticks.size() == 1)
+            return 0;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto x: sticks)
+            pq.push(x);
+        int minCost = 0;
+
+        while(pq.size() >= 2){
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            a = a + b;
+            minCost += a ;
+            // std::cout << a << " " << minCost << '\n'; 
+            pq.push(a);
+        }
+        // if(!pq.empty())
+        //     minCost += pq.top();
+
+        return minCost;
+    }
+};
